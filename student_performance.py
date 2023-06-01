766b0650b30136267de896604345ce43ade352c3
Adrian Saputra <91085948+Drians21@users.noreply.github.com>
2023-06-02 03:20:44 +0700

    Update student_performance.py

--- student_performance.py
+++ student_performance.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
